cd647b3654f438a26cc366e94e59944b0f943ddb
Oli Nelson <olivernelson@icloud.com>
2019-02-06 15:52:00 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,35 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name ,crew, phasers, shields){
+    this.name = name
+    this.phasers = phasers
+    this.shields = shields
+    this.crew = crew
+
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasersCharge = "uncharged"
+
+    if (this.crew.length) {
+      this.docked = false
+
+    }else{
+      this.docked = true
+    }
+
+    for (let member of this.crew){
+      member.currentShip = this
+    }
+
+
+
+    }
+
+    // docked(){
+    //   if (this.crew.length === 0 ){
+    //     return true
+    //   }else{
+    //     return false
+    //   }
+    // }
+
+}
