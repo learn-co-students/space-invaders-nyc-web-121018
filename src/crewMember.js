bf4cc092a5e65261651a2eaf8548079b4d97e9c6
Nison Malayev <nmalayev@gmail.com>
2019-02-06 15:44:11 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,30 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position, currentShip = 'Looking for a Rig'){
+    this.position = position;
+    this.currentShip = currentShip
+  } // end of constructor
+
+  engageWarpDrive(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    } else if (this.currentShip.crew.includes(this) && this.position === 'Pilot'){
+      this.currentShip.warpDrive = 'engaged'
+    }
+  }
+
+  chargePhasers() {
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    } else if (this.currentShip.crew.includes(this) && this.position === 'Gunner'){
+      this.currentShip.phasersCharge = 'charged'
+    }
+  }
+  setsInvisibility() {
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    } else if (this.currentShip.crew.includes(this) && this.position === 'Defender'){
+      this.currentShip.cloaked = true
+    }
+  }
+
+}
